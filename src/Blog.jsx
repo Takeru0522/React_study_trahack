ebb2201ff16f87b831f88b3fd8f19553504fd127
Takeru Kayamori <takeru.kayamori@gmail.com>
2020-07-01 01:00:07 +0800

    save

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -7,56 +7,40 @@ import * as FooBar from './components/FooBar';
 // 名前なし
 import Hoge from './components/Hoge';
 
-class Blog extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            isPublished: false,
-            count: 0,
-        }
-    }
+const Blog = () => {
+    // componentDidMount() {
+    //     // ボタンがクリックされたらいいねをカウントアップする　
+    //     document.getElementById('counter').addEventListener('click', this.countUp);
+    // }
+    // componentDidUpdate() {
+    //     // console.log(this.state.count)
+    //     if(this.state.count >= 10) {
+    //         this.setState({
+    //             count: 0
+    //         });
+    //     }
+    // }
+    // componentWillUnmount() {
+    //     document.getElementById('counter').removeEventListener('click', this.countUp);
+    // }
+    // countUp = () => {
+    //     this.setState({
+    //         count: this.state.count + 1
+    //     });
+    // }
+     
+    return (
+        <>
+            <Article 
+                title={'React Component'} 
+                // count={this.state.count}
+            />
+            {/* <Bar /> */}
+            <FooBar.Foo />
+            <FooBar.Bar />
+            <Hoge />
+        </>
+    );
 
-    componentDidMount() {
-        // ボタンがクリックされたらいいねをカウントアップする　
-        document.getElementById('counter').addEventListener('click', this.countUp);
-    }
-    componentDidUpdate() {
-        // console.log(this.state.count)
-        if(this.state.count >= 10) {
-            this.setState({
-                count: 0
-            });
-        }
-    }
-    componentWillUnmount() {
-        document.getElementById('counter').removeEventListener('click', this.countUp);
-    }
-
-    togglePublished = () => {
-        this.setState({
-            isPublished: !this.state.isPublished
-        })
-    };
-    countUp = () => {
-        this.setState({
-            count: this.state.count + 1
-        });
-    }
-    render() {        
-        return (
-            <>
-                <Article 
-                    title={'React Component'} 
-                    isPublished={this.state.isPublished} 
-                    toggle={() => this.togglePublished()} 
-                    count={this.state.count}
-                />
-                {/* <Bar /> */}
-                <FooBar.Foo />
-                <FooBar.Bar />
-                <Hoge />
-            </>
-        );
-    }
 }
 export default Blog;
--- src/LikeButton.jsx
+++ src/LikeButton.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 const LikeButton = (props) => {
+    const [count, counter] = useState(0);
+    const countUp = () => {
+        counter(count + 1)
+    }
+    useEffect(() => {
+        console.log('render')
+        document.getElementById('counter').addEventListener('click', countUp);
+        return() => {
+            console.log('Unmounting')
+            document.getElementById('counter').removeEventListener('click', countUp);
+        }
+    })
     return(
-        <button id={"counter"}>いいね数：{props.count}</button>
+        <button id={"counter"}>いいね数：{count}</button>
     );
 }
 export default LikeButton;
